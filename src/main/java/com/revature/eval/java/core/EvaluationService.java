3d25479cae8c927d3f3ee87dd827be056e68ac03
Wes Oler- Repository Environment <32803680+wes-o@users.noreply.github.com>
2019-11-07 22:03:36 -0600

    comment on question 1

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -14,6 +14,9 @@ public class EvaluationService {
 	 * @return
 	 */
 	public String reverse(String string) {
+		/* Check validity
+		 * if(string == null || string.equals(null)) { return string; }
+		 */
 		String input = string;
 		String reversed = "";
 		 
