ec0b188d2638fcc1ccb46041a634627f65eab790
wes-o <weso.turn@gmail.com>
2019-11-12 03:14:49 -0600

    added question eleven

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -19,9 +19,11 @@ public class EvaluationService {
 	 * @return
 	 */
 	public String reverse(String string) {
-		/*
-		 * Check validity if(string == null || string.equals(null)) { return string; }
-		 */
+		// Check validity 
+		if(string == null || string.equals(null)) { 
+			return string; 
+		}
+		
 		String input = string;
 		String reversed = "";
 
